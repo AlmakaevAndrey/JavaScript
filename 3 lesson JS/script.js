5b5b52cada1a0aa5ca487bdb5653e9450645fc59
Andrey <almakaevandrey@gmail.com>
2024-12-16 21:52:16 +0300

    Lesson 3

---
+++ 3 lesson JS/script.js
@@ -0,0 +1,48 @@
+// переменная - коробочка с данными
+
+// декларирование let x;
+// присваивание x = 1;
+
+// переменные могут быть в JS var, let, const;
+
+// var x1 = 1;
+// var x2 = 1;
+// const x3 = 1;
+
+// соглашения об именовании переменных 
+// только буквы, цифры, нижнее подчеркивание и знак доллара
+
+// console.log(x1, x2, x3);
+
+/* let a;
+a = 2; */
+// let a = 1, b = 1, c = 1;
+// let a, b, c;
+
+// console.log(a);
+// let a = 1;
+// var a = 1;
+
+// let a = 1;
+// a = 10;
+// console.log(a);
+
+// const b = 1;
+// b = 20;
+// console.log(b);
+
+// {
+//     const x = 100;
+//     console.log(x);
+// }
+
+// const firstName = 'Andrey';
+// const age = 25;
+
+// console.log(typeof firstName)
+// console.log(typeof age)
+
+{
+    var x = 1;
+}
+console.log(x)
