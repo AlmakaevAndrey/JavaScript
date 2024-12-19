963f140d5e55fa0dea544d9f23497a828943e46f
Andrey <almakaevandrey@gmail.com>
2024-12-19 22:13:17 +0300

    5 lesson

---
+++ 5 lesson JS/script5.js
@@ -0,0 +1,80 @@
+// Operators
+
+let lessons = 10;
+
+lessons = 10 + 10;
+lessons = 10 - 5;
+lessons = 10 * 10;
+lessons = 10 / 10;
+lessons = 11 % 2; // остаток от деления
+
+
+// console.log(lessons);
+// -------------
+
+let x;
+x = 2 ** 2; // возведение в степень
+x = 2 ** 3; // возведение в степень
+
+
+x = 1;
+x = x + 1; // x++
+x = x - 1; // x--
+
+// console.log(x);
+
+// -------------
+let a = 10;
+
+// let b = a++; // постфиксный инкремен 
+// let b = ++a; // префексный инкремен 
+// let b = a--; // постфиксный декремент 
+let b = --a;    // префиксный инкремен 
+
+
+// console.log(b);
+// console.log(a);
+
+// -------------
+// Операторы присваивания
+
+let d = 10;
+
+d += 1;
+d -= 1;
+d *= 1;
+d /= 1;
+d %= 1;
+d **= 2;
+
+// console.log(d);
+// Операторы сравнения
+// >, <, >=, ,<=
+// ==, ===, !=, !==
+
+// console.log(1 == 1);
+// console.log(1 === 1);
+// console.log(1 > 1);
+// console.log(1 < 1);
+// console.log(1 >= 1);
+// console.log(1 <= 1);
+
+// console.log(1 =="1");
+// console.log(1 === "1");
+
+console.log(1 != "1");
+console.log(1 !== "1");
+
+
+// -------------
+// Конкатинация (объединение)
+
+const greeting = "Hello" + "World!";
+
+console.log(greeting);
+// console.log(1 + " la la la");
+console.log(1 + "1");
+console.log(1 + 100);
+
+
+
